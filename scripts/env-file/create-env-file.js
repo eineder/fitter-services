4c4a33fc1ba06bb6d0b80e8a014ef16f80e85b57
eineder <49588296+eineder@users.noreply.github.com>
2024-07-01 07:07:20 +0200

    Finish script for creating .env file

--- scripts/env-file/create-env-file.js
+++ scripts/env-file/create-env-file.js
@@ -11,69 +11,36 @@ async function execute() {
   const pipelineName = `appsyncmasterclass_${getBranchName()}_pipeline`;
   const stages = await getPipelineStages(pipelineName);
   const testStage = stages.find((stage) => stage.name === "TEST");
+  // Get unique stack names of the test stage
   const testStageStackNames = new Set(
     testStage.actions.map((ts) => ts.configuration.StackName)
   );
 
-  const cfClient = new cf.CloudFormationClient();
+  const outputs = await getOutputs(testStageStackNames);
+
+  let envContent = "";
+  outputs.forEach((output) => {
+    const key = camelToSnakeCase(output.OutputKey);
+    const value = output.OutputValue;
+    envContent += `${key}=${value}\n`;
+  });
+  fs.writeFileSync(".env", envContent);
+}
+async function getOutputs(testStageStackNames) {
+  const client = new cf.CloudFormationClient();
+  const promises = [];
   for (const stackName of testStageStackNames) {
     const cmd = new cf.DescribeStacksCommand({
       StackName: stackName,
     });
-    const response = await cfClient.send(cmd);
-    return response.Stacks[0];
+    const promise = client.send(cmd);
+    promises.push(promise);
   }
-
-  console.log("Test stage:", testStage);
-  // for (const action of testStage.actions) {
-  //   // Assuming action configuration contains the stack name
-  //   // This part depends on how your pipeline is set up
-  //   const stackName = action.configuration.StackName;
-  //   if (stackName) {
-  //     const resources = await getStackResources(stackName);
-  //     console.log(`Resources in stack ${stackName}:`, resources);
-  //     // If you need to filter for sub-stacks, do so here by checking resource type
-  //   }
-  // }
-
-  // const subStacks = (await listSubStacks(pipelineName)).filter((s) =>
-  //   s.StackName.includes("AppSync")
-  // );
-  // const client = new cf.CloudFormationClient();
-
-  // const subStackDescs = subStacks.map(async (stack) => {
-  //   const cmd = new cf.DescribeStacksCommand({
-  //     StackName: stack.StackName,
-  //   });
-  //   const output = await client.send(cmd);
-  //   return output.Stacks[0];
-  // });
-
-  // for (const desc of subStackDescs) {
-  //   const outputs = desc.Outputs;
-  //   let envContent = "";
-  //   outputs.forEach((output) => {
-  //     envContent += `${desc.StackName}_${output.OutputKey}=${output.OutputValue}\n`;
-  //   });
-  //   fs.writeFileSync(".env", envContent);
-  // }
-
-  // client.send(cmd).then((data) => {
-  //   const pipelineStack = data.Stacks[0];
-  //   listSubStacks(pipelineStack.StackName).then((subStacks) => {
-  //     for (let stack of subStacks) {
-  //       const outputs = stack.Outputs;
-  //       let envContent = "";
-  //       outputs.forEach((output) => {
-  //         envContent += `${stack.}_${camelToSnakeCase(output.OutputKey)}=${
-  //           output.OutputValue
-  //         }\n`;
-  //       });
-  //       fs.writeFileSync(".env", envContent);
-  //     }
-  //   });
-  // });
+  const responses = await Promise.all(promises);
+  const outputs = responses.flatMap((response) => response.Stacks[0].Outputs);
+  return outputs.filter((output) => output !== undefined);
 }
+
 function getBranchName() {
   try {
     const branch = execSync("git rev-parse --abbrev-ref HEAD")
@@ -92,26 +59,8 @@ async function getPipelineStages(pipelineName) {
   return response.pipeline.stages;
 }
 
-async function listSubStacks(stackName) {
-  const client = new cf.CloudFormationClient();
-  const cmd = new cf.ListStackResourcesCommand({
-    StackName: stackName,
-  });
-
-  try {
-    const data = await client.send(cmd);
-    const subStacks = data.StackResourceSummaries.filter(
-      (resource) => resource.ResourceType === "AWS::CloudFormation::Stack"
-    );
-    return subStacks;
-  } catch (error) {
-    console.error("Error listing sub-stacks:", error);
-    throw error;
-  }
+function camelToSnakeCase(str) {
+  return str
+    .replace(/[A-Z]/g, (letter, index) => `${index > 0 ? "_" : ""}${letter}`)
+    .toUpperCase();
 }
-
-// function camelToSnakeCase(str) {
-//   return str
-//     .replace(/[A-Z]/g, (letter, index) => `${index > 0 ? "_" : ""}${letter}`)
-//     .toUpperCase();
-// }
