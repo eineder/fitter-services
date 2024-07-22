c356e7f0d6d72c825f9f56d69a429a4d3f8bb2f5
eineder <49588296+eineder@users.noreply.github.com>
2024-07-22 12:40:12 +0200

    Add some nice emojis

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,22 @@ export AWS_PAGER=""
 
 echo "**** Installing AWS CDK... ****"
 npm install -g aws-cdk || exit 1
-echo "**** Installed AWS CDK ****"
+echo "**** ✅ Successfully installed AWS CDK ****"
 
 echo "**** Deploying TEST stage... ****"
 cdk deploy "TEST/*" --require-approval never || exit 1
-echo "**** Deployed TEST stage ****"
+echo "**** ✅ Successfully deployed TEST stage ****"
 
 echo "**** Creating .env file... ****"
 cd scripts/env-file
 npm ci || exit 1
 cd ../..
 node ./scripts/env-file/create-env-file.js || exit 1
-echo "**** Created .env file ****"
+echo "**** ✅ Successfully created .env file ****"
 
 echo "**** Loading environment variables to be available in the following commands... ****"
 source .env || exit 1
-echo "**** Loaded environment variables ****"
+echo "**** ✅ Successfully loaded environment variables ****"
 
 echo "**** Invoking lambda function to seed database... ****"
 aws lambda invoke \
@@ -26,22 +26,23 @@ aws lambda invoke \
     --payload '{}' \
     --invocation-type RequestResponse \
     lambda-out.json || exit 1
+echo "**** ✅ Successfully invoked lambda function to seed database... ****"
 
 # Check if the output is null - exit if not
 if ! grep -q "null" lambda-out.json; then
-  echo "**** Lambda function output is not null as expected: ****"
+  echo "**** ❌ Lambda function output is not null as expected: ****"
   cat lambda-out.json
   echo "**** Exiting... ****"
   exit 1
 fi
-echo "**** Table seeded successfully: $SWEARWORDS_TABLE_NAME ****"
+echo "**** ✅ Successfully seeded table $SWEARWORDS_TABLE_NAME ****"
 
 
 echo "**** Running tests... ****"
 go test -count=1 ./... || exit 1
 # -count=1 to avoid caching
-echo "**** Successfully ran tests ****"
+echo "**** ✅ Successfully ran tests ****"
 
 echo "**** Deploying PROD stage... ****"
 cdk deploy "PROD/*" --require-approval never || exit 1
-echo "**** Deployed PROD stage ****"
+echo "**** ✅ Successfully deployed PROD stage ****"
