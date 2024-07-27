94ce4419fbf3c0262f686cb941b2556c82541004
eineder <49588296+eineder@users.noreply.github.com>
2024-07-28 01:05:20 +0200

    Use dynamo batch operations to make seeding of DB faster

--- swearwords/prime_swearwords/prime_swearwords.go
+++ swearwords/prime_swearwords/prime_swearwords.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -22,11 +24,11 @@ func HandleRequest(ctx context.Context, event *any) (any, error) {
 	fmt.Printf("%+v\n", event)
 	fmt.Println("Prime_Swearwords called")
 
-	session, err := session.NewSession()
-	if err != nil {
-		fmt.Printf("Got error creating session: %s", err)
-		return nil, err
-	}
+	session := session.Must(session.NewSession(&aws.Config{
+		HTTPClient: &http.Client{
+			Timeout: 30 * time.Second, // Increase the timeout to 30 seconds
+		},
+	}))
 
 	db := dynamodb.New(session)
 
@@ -52,31 +54,39 @@ func EnsureTablePrimed(db *dynamodb.DynamoDB, session *session.Session) error {
 
 	bucket := aws.String(os.Getenv("BUCKET_NAME"))
 	key := aws.String(os.Getenv("BUCKET_KEY"))
+	fmt.Printf("getting file content from bucket %s and key %s\n", *bucket, *key)
 	content, readErr := getFileContent(bucket, key, session)
 	if readErr != nil {
 		fmt.Printf("Got no file content")
 		return readErr
 	}
+	fmt.Println("Got file content")
 
 	// Split content into lines
 	lines := strings.Split(*content, "\n")
+	fmt.Printf("Adding  %d words to table\n", len(lines))
+
+	var items []*dynamodb.WriteRequest
 	for _, line := range lines {
-		_, err := db.PutItem(&dynamodb.PutItemInput{
-			TableName: aws.String(os.Getenv("SWEARWORDS_TABLE_NAME")),
-			Item: map[string]*dynamodb.AttributeValue{
-				"word": {
-					S: aws.String(line),
+		item := &dynamodb.WriteRequest{
+			PutRequest: &dynamodb.PutRequest{
+				Item: map[string]*dynamodb.AttributeValue{
+					"word": {
+						S: aws.String(line),
+					},
 				},
 			},
-		})
-		if err != nil {
-			fmt.Printf("Got error calling PutItem: %s", err)
-			return err
 		}
-
+		items = append(items, item)
 	}
 
-	_, err2 := db.PutItem(&dynamodb.PutItemInput{
+	err = BatchWriteItems(db, items)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Added  %d words to table\n", len(lines))
+
+	_, err = db.PutItem(&dynamodb.PutItemInput{
 		TableName: aws.String(os.Getenv("SWEARWORDS_TABLE_NAME")),
 		Item: map[string]*dynamodb.AttributeValue{
 			"word": {
@@ -84,11 +94,32 @@ func EnsureTablePrimed(db *dynamodb.DynamoDB, session *session.Session) error {
 			},
 		},
 	})
-	if err2 != nil {
-		fmt.Printf("Got error calling PutItem: %s", err2)
-		return err2
+	if err != nil {
+		fmt.Printf("Got error calling PutItem: %s", err)
+		return err
 	}
 
+	return nil
+}
+func BatchWriteItems(db *dynamodb.DynamoDB, items []*dynamodb.WriteRequest) error {
+	SWEARWORDS_TABLE_NAME := os.Getenv("SWEARWORDS_TABLE_NAME")
+	var batchItems []*dynamodb.WriteRequest
+	for i, item := range items {
+		batchItems = append(batchItems, item)
+		if len(batchItems) == 25 || i == len(items)-1 {
+			batchInput := &dynamodb.BatchWriteItemInput{
+				RequestItems: map[string][]*dynamodb.WriteRequest{
+					SWEARWORDS_TABLE_NAME: batchItems,
+				},
+			}
+			_, err := db.BatchWriteItem(batchInput)
+			if err != nil {
+				fmt.Printf("Got error calling BatchWriteItem: %s", err)
+				return err
+			}
+			batchItems = nil
+		}
+	}
 	return nil
 }
 
@@ -114,11 +145,14 @@ func isTablePrimed(db *dynamodb.DynamoDB) (bool, error) {
 func getFileContent(bucket *string, key *string, session *session.Session) (*string, error) {
 
 	s3svc := s3.New(session)
+	fmt.Println("Created S3 service")
 
+	fmt.Printf("Getting object %s from bucket %s\n", *key, *bucket)
 	result, err := s3svc.GetObject(&s3.GetObjectInput{
 		Bucket: bucket,
 		Key:    key,
 	})
+	fmt.Println("Got object")
 	if err != nil {
 		fmt.Printf("Got error calling GetObject: %s", err)
 		return nil, err
@@ -127,11 +161,13 @@ func getFileContent(bucket *string, key *string, session *session.Session) (*str
 	defer result.Body.Close()
 
 	// Read the file contents into a byte slice
+	fmt.Println("Reading file")
 	fileBytes, err := ioutil.ReadAll(result.Body)
 	if err != nil {
 		fmt.Printf("Got error reading file: %s", err)
 		return nil, err
 	}
+	fmt.Println("Read file")
 
 	// Convert the byte slice to a string
 	fileContents := string(fileBytes)
